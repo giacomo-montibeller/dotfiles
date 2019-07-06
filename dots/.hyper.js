8bccbad7e6ac78a9707a2022ad2de4d3396f5ea0
danielefongo <danielefongo@gmail.com>
2019-07-06 12:36:14 +0200

    add hyper terminal

---
+++ dots/.hyper.js
@@ -0,0 +1,15 @@
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 18,
+    fontFamily: 'Source Code Pro',
+    fontWeight: 'normal',
+    bell: 'false',
+    selectionColor: '#FFF'
+  },
+
+  plugins: [
+  'hyper-search', 'hyperline', 'hypercwd', 'hyperterm-tabs', "hyper-snazzy", 'hyper-solarized-dark-highcontrast'],
+
+  localPlugins: []
+};
